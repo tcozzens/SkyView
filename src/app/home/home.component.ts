862878765560735b973cdd363c6839805c9319dd
tcozzens <tcozzens91@gmail.com>
2017-01-04 21:44:04 -0500

    Added scroll indicator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,12 +26,14 @@ export class HomeComponent {
     if (window.scrollY > 50 && this.originalScroll > window.scrollY){
       this.originalScroll = window.scrollY;
       this.appComponent.navbarMove = 'fixed';    
+      this.appComponent.scrollHintMove = 'arrow-wrap';    
       this.topPadding = 'paddingTop50';
       console.log("SCROLLED UP: ");
     }
     else if (window.scrollY > 50 && this.originalScroll < window.scrollY){
       this.originalScroll = window.scrollY;
       this.appComponent.navbarMove = 'nav-up';
+      this.appComponent.scrollHintMove = 'nav-up';
       this.topPadding = 'paddingTop0';
       console.log("SCROLLED DOWN: ");
     }
