3a029224a7dcb78de05842461acb1ecd1d152af2
tcozzens <tcozzens91@gmail.com>
2017-01-05 22:28:00 -0500

    Added image loop

--- src/app/real-estate/real-estate.component.ts
+++ src/app/real-estate/real-estate.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HostListener } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 import { AppComponent } from '../app.component';
 
@@ -11,7 +12,62 @@ import { AppComponent } from '../app.component';
 
 export class RealEstateComponent {
   title = 'RealeEstate works!';
-  public image =  "/assets/img/real-estate/aerial-photo.jpg" ;
+  public currentImage = 0;
+  public currentImagePath = images[this.currentImage].location;
+  public currentImageType = images[this.currentImage].type;
+  public passSecurityForEmbeded;
+  public isSrcImage = 'srcIsImage';
+  public isSrcVideo = 'srcIsNotVideo';
+  imageHeight;
+  imageWidth;
 
-  constructor () {}
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+
+  nextImage() {
+    this.imageHeight = document.getElementById('image').clientHeight;
+    this.imageWidth = document.getElementById('image').clientWidth;
+    this.currentImage++;
+    this.currentImagePath = images[this.currentImage].location;
+    this.currentImageType = images[this.currentImage].type;
+
+    if (this.currentImageType === 'image') {
+      this.isSrcImage = 'srcIsImage';
+      this.isSrcVideo = 'srcIsNotVideo';
+    } else {
+      this.isSrcVideo = 'srcIsVideo';
+      this.isSrcImage = 'srcIsNotImage';
+      this.passSecurityForEmbeded = this.domSanitizer.bypassSecurityTrustResourceUrl(this.currentImagePath);
+    }
+  }
+
+  previousImage() {
+    this.imageHeight = document.getElementById('image').clientHeight;
+    this.imageWidth = document.getElementById('image').clientWidth;
+    this.currentImage--;
+    this.currentImagePath = images[this.currentImage].location;
+    this.currentImageType = images[this.currentImage].type;
+
+    if (this.currentImageType === 'image') {
+      this.isSrcImage = 'srcIsImage';
+      this.isSrcVideo = 'srcIsNotVideo';
+    } else {
+      this.isSrcVideo = 'srcIsVideo';
+      this.isSrcImage = 'srcIsNotImage';
+      this.passSecurityForEmbeded = this.domSanitizer.bypassSecurityTrustResourceUrl(this.currentImagePath);
+    }
+  }
 }
+
+var images = [
+  {
+    index: 0,
+    location: '/assets/img/real-estate/aerial-photo.jpg',
+    type: 'image'
+  },
+  {
+    index: 1,
+    location: 'https://www.youtube.com/embed/VG8Edeh2pl4',
+    type: 'video'
+  }
+]
